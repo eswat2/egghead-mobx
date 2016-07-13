60b1fe2f23fbf381384c41b5acbc4941674e2b49
eswat2 <eswat2@gmail.com>
2016-07-13 14:31:58 -0700

    refactor add note input to use a form

--- app/components/Notes/AddNote.jsx
+++ app/components/Notes/AddNote.jsx
@@ -6,15 +6,16 @@ class AddNote extends React.Component {
     super(props);
     this.input = null;
 
-    this.setRef = (ref) => this._setRef(ref);
-    this.handleSubmit = () => this._handleSubmit();
+    this.getRef = (ref) => this._getRef(ref);
+    this.handleSubmit = (event) => this._handleSubmit(event);
   }
 
-  _setRef(ref) {
+  _getRef(ref) {
     this.input = ref;
   }
 
-  _handleSubmit() {
+  _handleSubmit(event) {
+    event.preventDefault();
     let newNote = this.input.value;
     this.input.value = '';
     actions.addNote(newNote);
@@ -23,24 +24,22 @@ class AddNote extends React.Component {
   render() {
     // console.log('-- render:  AddNote');
     return (
-      <div className="input-group">
-        <div className="input-group-addon"><i className="fa fa-chevron-right"></i></div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Note..."
-          ref={this.setRef}
-          />
-        <span className="input-group-btn">
-          <button
-            className="btn btn-warning"
-            type="button"
-            onClick={this.handleSubmit}
-            >
-            Add <i className="fa fa-sticky-note"></i>
-          </button>
-        </span>
-      </div>
+      <form onSubmit={this.handleSubmit} >
+        <div className="input-group">
+          <div className="input-group-addon"><i className="fa fa-chevron-right"></i></div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Note..."
+            ref={this.getRef}
+            />
+          <span className="input-group-btn">
+            <button className="btn btn-warning" type="submit">
+              Add <i className="fa fa-sticky-note"></i>
+            </button>
+          </span>
+        </div>
+      </form>
     )
   }
 }
